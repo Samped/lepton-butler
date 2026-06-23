5d814119faae1c1a1c09a673facfcd24f9a95f6e
samped <michaelsamuelpedro@gmail.com>
2026-06-23 23:07:35 +0100

    export delegation package surface

---
+++ packages/delegation/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./arc-env.ts";
+export * from "./clients.ts";
+export * from "./environment.ts";
+export * from "./deploy.ts";
+export * from "./build-delegation.ts";
+export * from "./accounts.ts";
+export * from "./redeem.ts";
